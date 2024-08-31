28257378d80c995cd475db8093d5a38f5bdb4296
GuiAz <guicottas@hotmail.com>
2024-08-31 13:04:04 -0300

    Iniciando o projeto IMDB...

---
+++ imdb/src/com/imdb/data/Midia.java
@@ -0,0 +1,39 @@
+package com.imdb.data;
+
+public class Midia {
+
+
+		private String titulo;
+		private int ano;
+		private double nota;
+		
+		public Midia() {}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public int getAno() {
+			return ano;
+		}
+
+		public void setAno(int ano) {
+			this.ano = ano;
+		}
+
+		public double getNota() {
+			return nota;
+		}
+
+		public void setNota(double nota) {
+			this.nota = nota;
+		}
+		
+
+	}
+
+
