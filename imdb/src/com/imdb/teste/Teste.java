60aecaf761bdfe145edc14b128d5b21be94fef52
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-14 12:26:43 -0300

    Sistema de cadastro com lista ativado.

--- imdb/src/com/imdb/teste/Teste.java
+++ imdb/src/com/imdb/teste/Teste.java
@@ -1,7 +1,11 @@
 package com.imdb.teste;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.imdb.data.Cadastro;
-import com.imdb.data.Midia;
+import com.imdb.data.Filme;
+import com.imdb.data.Serie;
 import com.imdb.users.Admin;
 
 public class Teste {
@@ -9,9 +13,62 @@ public class Teste {
 	public static void main(String[] args) {
 
 		Cadastro cadastro = new Cadastro();
+		
 		Admin admin = null;
+		//Criando o Administrador
+		admin = new Admin();
+		admin.setCredencial("A");
+		admin.setNome("Basualdo");
+		admin.setEmail("basualdo@email.com");
+		admin.setId(123456);
 		cadastro.setAdmin(admin);
 		
+		Filme filme = new Filme();
+		filme.setAno(2024);
+		filme.setBilheteria(10000);
+		filme.setDuracao(3.50D);
+		filme.setNota(10);
+		filme.setTitulo("DeadPool e Wolwerine");
+		cadastro.setFilme(filme);
+				
+		Serie serie = new Serie();
+		serie.setAno(2022);
+		serie.setTemporada(6);
+		serie.setEpisodios(65);
+		serie.setNota(10);
+		serie.setTitulo("Cobra Kai");
+		cadastro.setSerie(serie);
+		
+		List<Cadastro> lista = new ArrayList<Cadastro>();
+		
+		lista.add(cadastro);
+		
+		//==================================
+		
+		cadastro = new Cadastro();
+		cadastro.setAdmin(admin);
+		
+		filme = new Filme();
+		filme.setAno(2015);
+		filme.setBilheteria(2000);
+		filme.setDuracao(2.50D);
+		filme.setNota(5);
+		filme.setTitulo("Os Outros");
+		cadastro.setFilme(filme);
+				
+		serie = new Serie();
+		serie.setAno(2020);
+		serie.setTemporada(4);
+		serie.setEpisodios(40);
+		serie.setNota(4);
+		serie.setTitulo("UPLOAD");
+		cadastro.setSerie(serie);
+		
+		lista.add(cadastro);
+		
+		for (Cadastro cad : lista) {
+			System.out.println("Nome do Filme: " + cad.getFilme().getTitulo());
+		}
 		
 
 	}
