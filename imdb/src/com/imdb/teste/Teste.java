d955ea3912ac64b5214cf28f2bfb2bad1e74f8db
GuiAz <guicottas@hotmail.com>
2024-09-14 13:02:45 -0300

    Iniciando tratativas de erro nas classes java.

--- imdb/src/com/imdb/teste/Teste.java
+++ imdb/src/com/imdb/teste/Teste.java
@@ -1,7 +1,9 @@
 package com.imdb.teste;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Scanner;
 
 import com.imdb.data.Cadastro;
 import com.imdb.data.Filme;
@@ -14,74 +16,95 @@ public class Teste {
 
 	public static void main(String[] args) {
 		
-		Cadastro cadastro = new Cadastro();	
-		
-		Admin admin = null;
-		//Criando o Administrador
-		admin = new Admin();
-		admin.setCredencial("A");
-		admin.setNome("Basualdo");
-		admin.setEmail("basualdo@email.com");
-		admin.setId(123456);		
-		cadastro.setAdmin(admin);	
-		
-		Filme filme = null;
-		//Criando o Filme
-		filme = new Filme();
-		filme.setAno(2024);
-		filme.setBilheteria(100000);
-		filme.setDuracao(3.50D);
-		filme.setNota(10);
-		filme.setTitulo("Deadpool e Wolverine");		
-		cadastro.setFilme(filme);		
-		
-		Serie serie = null;
-		serie = new Serie();
-		serie.setAno(2022);
-		serie.setTemporada(6);
-		serie.setEpisodios(65);
-		serie.setNota(6);
-		serie.setTitulo("Cobra Kai");		
-		cadastro.setSerie(serie);
-		
-//		ArrayList al = new ArrayList<String>();
+//		Cadastro cadastro = new Cadastro();	
 //		
-//		al.add("Carla");
-//		al.add("Nalva");
-//		al.add("Maria");
+//		Admin admin = null;
+//		//Criando o Administrador
+//		admin = new Admin();
+//		admin.setCredencial("A");
+//		admin.setNome("Basualdo");
+//		admin.setEmail("basualdo@email.com");
+//		admin.setId(123456);		
+//		cadastro.setAdmin(admin);	
+//		
+//		Filme filme = null;
+//		//Criando o Filme
+//		filme = new Filme();
+//		filme.setAno(2024);
+//		filme.setBilheteria(100000);
+//		filme.setDuracao(3.50D);
+//		filme.setNota(10);
+//		filme.setTitulo("Deadpool e Wolverine");		
+//		cadastro.setFilme(filme);		
+//		
+//		Serie serie = null;
+//		serie = new Serie();
+//		serie.setAno(2022);
+//		serie.setTemporada(6);
+//		serie.setEpisodios(65);
+//		serie.setNota(6);
+//		serie.setTitulo("Cobra Kai");		
+//		cadastro.setSerie(serie);
+//		
+//		
+//		List<Cadastro> lista = new ArrayList<Cadastro>();
+//		
+//		lista.add(cadastro);
+//		
+//		cadastro = new Cadastro();
+//		cadastro.setAdmin(admin);
+//	
+//		filme = new Filme();
+//		filme.setAno(2020);
+//		filme.setBilheteria(200000);
+//		filme.setDuracao(3.20D);
+//		filme.setNota(10);
+//		filme.setTitulo("Interestelar");		
+//		cadastro.setFilme(filme);		
+//		
+//		serie = new Serie();
+//		serie.setAno(2015);
+//		serie.setTemporada(8);
+//		serie.setEpisodios(80);
+//		serie.setNota(10);
+//		serie.setTitulo("Breaking Bad");		
+//		cadastro.setSerie(serie);
+//		
+//		lista.add(cadastro);
+//		
+//		for (Cadastro cad : lista) {
+//			System.out.println("Nome do Filme: " + cad.getFilme().getTitulo());
+//		}
 		
-		List<Cadastro> lista = new ArrayList<Cadastro>();
 		
-		lista.add(cadastro);
+		//Erros
+		//Exceções
 		
-		cadastro = new Cadastro();
-		cadastro.setAdmin(admin);
-	
-		filme = new Filme();
-		filme.setAno(2020);
-		filme.setBilheteria(200000);
-		filme.setDuracao(3.20D);
-		filme.setNota(10);
-		filme.setTitulo("Interestelar");		
-		cadastro.setFilme(filme);		
+		Scanner scan = new Scanner(System.in);
 		
-		serie = new Serie();
-		serie.setAno(2015);
-		serie.setTemporada(8);
-		serie.setEpisodios(80);
-		serie.setNota(10);
-		serie.setTitulo("Breaking Bad");		
-		cadastro.setSerie(serie);
+		int nr1 = 0;
+		int nr2 = 0;
 		
-		lista.add(cadastro);
-		
-		for (Cadastro cad : lista) {
-			System.out.println("Nome do Filme: " + cad.getFilme().getTitulo());
+		try {
+			System.out.println("Digite o valor do primeiro número: ");
+			nr1 = Integer.parseInt(scan.next());		
+			System.out.println("Digite o valor do segundo número: ");		
+			nr2 = Integer.parseInt(scan.next());;
+		} catch(NumberFormatException e) {
+			System.out.println("Somente numero podem ser digitados: ");
+			nr1 = Integer.parseInt(scan.next());		
+			System.out.println("Digite o valor do segundo número: ");		
+			nr2 = Integer.parseInt(scan.next());;
 		}
 		
+		int resultado = 0;
+		
+		resultado = nr1 + nr2;
+		
+		System.out.println("O resultado da soma foi: " + resultado);;
 		
 		
-		System.out.println("Nome do Admin: " + cadastro.getAdmin().getNome());
+		
 
 	}
 
