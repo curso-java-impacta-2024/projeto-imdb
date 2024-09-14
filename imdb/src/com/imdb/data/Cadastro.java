0c0ab9fd2cfe13e0af4eaae28e6dca8bcd85da37
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-14 11:16:40 -0300

    Classe cadastro adicionada

---
+++ imdb/src/com/imdb/data/Cadastro.java
@@ -0,0 +1,39 @@
+package com.imdb.data;
+
+import com.imdb.users.Admin;
+
+public class Cadastro {
+		
+	private Admin admin;
+	private Filme filme;
+	private Serie serie;
+	
+	public Cadastro() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+
+	public Serie getSerie() {
+		return serie;
+	}
+
+	public void setSerie(Serie serie) {
+		this.serie = serie;
+	}
+	
+}
--- imdb/src/com/imdb/teste/Teste.java
+++ imdb/src/com/imdb/teste/Teste.java
@@ -1,17 +1,16 @@
 package com.imdb.teste;
 
+import com.imdb.data.Cadastro;
 import com.imdb.data.Midia;
+import com.imdb.users.Admin;
 
 public class Teste {
 
 	public static void main(String[] args) {
-		//Instânciando uma classe.
-		Midia midia;
-		//Constuindo o objeto a partir da instância.
-		midia = new Midia();
-		
-		//Instanciar e construir ao mesmo tempo.
-		Midia novaMidia = new Midia();
+
+		Cadastro cadastro = new Cadastro();
+		Admin admin = null;
+		cadastro.setAdmin(admin);
 		
 		
 
